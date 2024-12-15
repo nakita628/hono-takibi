896f878f13684808ee89c3ef0814e3626a0958ec
nakita-Ypm <nakita@ypm.llc>
2024-12-15 07:38:41 +0000

    add Hono Takibi main

--- packages/hono-takibi/src/index.ts
+++ packages/hono-takibi/src/index.ts
@@ -1,1 +1,81 @@
-console.log('Hono Takibi🔥')
+#!/usr/bin/env node
+
+import SwaggerParser from '@apidevtools/swagger-parser'
+import fs from 'fs'
+import path from 'path'
+import { format } from 'prettier'
+import { generateHono } from './generators/hono/generate-hono'
+import { OpenAPISpec } from './types'
+
+/**
+ * CLI entry point for hono-takibi
+ *
+ * Usage:
+ * ```
+ * hono-takibi <input-file> [-o output-file]
+ * ```
+ *
+ * @example
+ * ```bash
+ * # Global install
+ * npm install -g hono-takibi
+ * hono-takibi openapi.yaml -o routes.ts
+ *
+ * # NPX usage
+ * npx hono-takibi openapi.yaml -o routes.ts
+ * ```
+ */
+export async function main(dev: boolean = false) {
+  // 1. argv ['**/bin/node', '**/dist/index.js', 'example/pet-store.yaml', '-o', 'routes/petstore-index.ts']
+  // 2. slice [ 'example/pet-store.yaml', '-o', 'routes/petstore-index.ts' ]
+  const args = process.argv.slice(2)
+  // 3. input = 'example/pet-store.yaml'
+  const input = args[0]
+  // 4. output = 'routes/petstore-index.ts'
+  const output = args[args.indexOf('-o') + 1]
+  try {
+    // 5. parse OpenAPI YAML or JSON
+    const openAPI = (await SwaggerParser.parse(input)) as OpenAPISpec
+    // 6. generate Hono code
+    const hono = generateHono(openAPI)
+    // 7. format code
+    const formattedCode = await format(hono, {
+      parser: 'typescript',
+      singleQuote: true,
+      semi: false,
+    })
+    // 8. write to file
+    if (output) {
+      // 8.1 output routes/petstore-index.ts
+      const outputDir = path.dirname(output)
+      // 8.2 outputDir routes
+      if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir, { recursive: true })
+      }
+    }
+    // 9. write to file
+    fs.writeFileSync(output, formattedCode, { encoding: 'utf-8' })
+    console.log(`Generated code written to ${output}`)
+    return true
+  } catch (e) {
+    console.error('Usage: hono-takibi <input-file> [-o output-file]')
+    if (e instanceof Error) {
+      console.error('Error processing OpenAPI document:', e.message)
+      if (dev) {
+        throw e
+      }
+    }
+    if (dev) {
+      throw new Error('Unknown error occurred')
+    }
+    return false
+  }
+}
+
+if (require.main === module) {
+  main().then((success) => {
+    if (!success) {
+      process.exit(1)
+    }
+  })
+}
