620dea5aa0cfb3eeccdea45de760e39d9e77a096
nakita-Ypm <nakita@ypm.llc>
2024-12-17 11:07:32 +0000

    add get-camel-case-schema.ts

---
+++ packages/hono-takibi/src/core/schema/references/get-camel-case-schema-name.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, it } from 'vitest'
+import { getCamelCaseSchemaName } from './get-camel-case-schema-name'
+
+const getCamelCaseSchemaNameTestCases = [
+  {
+    schemaName: 'Order',
+    expected: 'orderSchema',
+  },
+  {
+    schemaName: 'Address',
+    expected: 'addressSchema',
+  },
+  {
+    schemaName: 'Customer',
+    expected: 'customerSchema',
+  },
+  {
+    schemaName: 'Category',
+    expected: 'categorySchema',
+  },
+  {
+    schemaName: 'User',
+    expected: 'userSchema',
+  },
+  {
+    schemaName: 'Tag',
+    expected: 'tagSchema',
+  },
+  {
+    schemaName: 'Pet',
+    expected: 'petSchema',
+  },
+  {
+    schemaName: 'ApiResponse',
+    expected: 'apiResponseSchema',
+  },
+]
+
+describe('getCamelCaseSchemaName', () => {
+  it.concurrent.each(getCamelCaseSchemaNameTestCases)(
+    `getCamelCaseSchemaName($schemaName) -> $expected`,
+    ({ schemaName, expected }) => {
+      const result = getCamelCaseSchemaName(schemaName)
+      expect(result).toBe(expected)
+    },
+  )
+})
