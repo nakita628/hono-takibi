9f96885e316949fcf8bf7453d0ef6bb781cbf351
nakita628 <nakita@ypm.llc>
2024-12-30 07:00:32 +0000

    fmt

--- packages/hono-takibi/src/generators/zod/generate-zod-property-schema.test.ts
+++ packages/hono-takibi/src/generators/zod/generate-zod-property-schema.test.ts
@@ -101,8 +101,7 @@ const generatePropertySchemaTestCases: { schema: Schema; expected: string }[] = 
   // uuid
   {
     schema: { type: 'string', format: 'uuid', example: '123e4567-e89b-12d3-a456-426614174000' },
-    expected:
-      'z.string().uuid().openapi({example:"123e4567-e89b-12d3-a456-426614174000"})',
+    expected: 'z.string().uuid().openapi({example:"123e4567-e89b-12d3-a456-426614174000"})',
   },
 ]
 
