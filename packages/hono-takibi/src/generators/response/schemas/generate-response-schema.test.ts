f52e642473fc5b3a70e56fb994f530d8c9f7cee4
nakita628 <nakita@ypm.llc>
2025-01-03 05:18:31 +0000

    test fix

--- packages/hono-takibi/src/generators/response/schemas/generate-response-schema.test.ts
+++ packages/hono-takibi/src/generators/response/schemas/generate-response-schema.test.ts
@@ -1,23 +1,76 @@
 import { describe, expect, it } from 'vitest'
 import { generateResponseSchema } from './generate-response-schema'
-import type { Content } from '../../../types'
+import type { Responses } from '../../../types'
 
-const generateResponseSchemaTestCases: { content: Content; expected: string }[] = [
+const generateResponseSchemaTestCases: { responses: Responses; expected: string }[] = [
   {
-    content: {
-      'application/json': {
-        schema: { type: 'object', properties: { message: { type: 'string', example: 'Hono🔥' } } },
+    responses: {
+      '200': {
+        description: 'Successful response with a welcome message.',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                message: {
+                  type: 'string',
+                  example: 'Hono🔥',
+                },
+              },
+              required: ['message'],
+            },
+          },
+        },
       },
     },
-    expected: `200:{description:'Hono🔥',content:{'application/json':{schema:z.object({message:z.string().openapi({example:"Hono🔥"}).optional()})}},},`,
+    expected: `200:{description:'Successful response with a welcome message.',content:{'application/json':{schema:z.object({message:z.string().openapi({example:"Hono🔥"})})}},},`,
+  },
+  {
+    responses: {
+      '200': {
+        description: 'successful operation',
+        headers: {
+          'X-Rate-Limit': {
+            description: 'calls per hour allowed by the user',
+            schema: {
+              type: 'integer',
+              format: 'int32',
+            },
+          },
+          'X-Expires-After': {
+            description: 'date in UTC when token expires',
+            schema: {
+              type: 'string',
+              format: 'date-time',
+            },
+          },
+        },
+        content: {
+          'application/xml': {
+            schema: {
+              type: 'string',
+            },
+          },
+          'application/json': {
+            schema: {
+              type: 'string',
+            },
+          },
+        },
+      },
+      '400': {
+        description: 'Invalid username/password supplied',
+      },
+    },
+    expected: `200:{description:'successful operation',content:{'application/xml':{schema:z.string()},'application/json':{schema:z.string()}},},400:{description:'Invalid username/password supplied',},`,
   },
 ]
 
 describe('generateResponseSchema', () => {
   it.concurrent.each(generateResponseSchemaTestCases)(
-    'generateResponseSchema(%content) -> %expected',
-    ({ content, expected }) => {
-      const result = generateResponseSchema({ '200': { description: 'Hono🔥', content } })
+    'generateResponseSchema(%responses) -> %expected',
+    ({ responses, expected }) => {
+      const result = generateResponseSchema(responses)
       expect(result).toBe(expected)
     },
   )
