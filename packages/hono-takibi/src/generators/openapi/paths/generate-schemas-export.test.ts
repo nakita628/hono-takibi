bfcbeec1b0a7683fe1cd46099cdd2dba042410c5
nakita628 <nakita@ypm.llc>
2025-01-05 02:59:47 +0000

    add pascal test case

--- packages/hono-takibi/src/generators/openapi/paths/generate-schemas-export.test.ts
+++ packages/hono-takibi/src/generators/openapi/paths/generate-schemas-export.test.ts
@@ -1,7 +1,11 @@
 import { describe, expect, it } from 'vitest'
 import { generateSchemasExport } from './generate-schemas-export'
 
-const generateSchemasExportTestCases = [
+const generateSchemasExportTestCases: {
+  orderedSchemas: string[]
+  namingCase?: 'camelCase' | 'PascalCase'
+  expected: string
+}[] = [
   {
     orderedSchemas: [
       'Order',
@@ -24,13 +28,36 @@ petSchema,
 apiResponseSchema
 }`,
   },
+  {
+    orderedSchemas: [
+      'Order',
+      'Address',
+      'Customer',
+      'Category',
+      'User',
+      'Tag',
+      'Pet',
+      'ApiResponse',
+    ],
+    namingCase: 'PascalCase',
+    expected: `export const schemas = {
+OrderSchema,
+AddressSchema,
+CustomerSchema,
+CategorySchema,
+UserSchema,
+TagSchema,
+PetSchema,
+ApiResponseSchema
+}`,
+  },
 ]
 
 describe('generateSchemasExport', () => {
   it.concurrent.each(generateSchemasExportTestCases)(
-    'generateSchemasExport($orderedSchemas) -> $expected',
-    ({ orderedSchemas, expected }) => {
-      const result = generateSchemasExport(orderedSchemas)
+    'generateSchemasExport($orderedSchemas, $namingCase) -> $expected',
+    ({ orderedSchemas, namingCase, expected }) => {
+      const result = generateSchemasExport(orderedSchemas, namingCase)
       expect(result).toBe(expected)
     },
   )
