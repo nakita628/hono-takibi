e3419aea21d9209a81ddea20b070f207ea4228d5
nakita628 <nakita@ypm.llc>
2025-01-14 10:50:00 +0000

    apply getVariableSchemaNameHelper

--- packages/hono-takibi/src/generators/openapi/components/allof/generate-allof-code.ts
+++ packages/hono-takibi/src/generators/openapi/components/allof/generate-allof-code.ts
@@ -1,6 +1,7 @@
 import type { Schema } from '../../../../types'
 import type { Config } from '../../../../config'
 import { generateZodSchema } from '../../../zod/generate-zod-schema'
+import { getVariableSchemaNameHelper } from '../../../../core/helper/get-variable-schema-name-helper'
 
 export function generateAllOfCode(schema: Schema, config: Config): string {
   if (!schema.allOf || schema.allOf.length === 0) {
@@ -11,7 +12,8 @@ export function generateAllOfCode(schema: Schema, config: Config): string {
       if (subSchema.$ref) {
         const refParts = subSchema.$ref.split('/')
         const refName = refParts[refParts.length - 1]
-        return `${refName}Schema`
+        const schemaName = getVariableSchemaNameHelper(refName, config)
+        return schemaName
       } else {
         return generateZodSchema(config, subSchema)
       }
---
+++ packages/hono-takibi/src/generators/openapi/components/oneof/generate-oneof-code.ts
@@ -0,0 +1,24 @@
+import type { Schema } from '../../../../types'
+import type { Config } from '../../../../config'
+import { generateZodSchema } from '../../../zod/generate-zod-schema'
+import { getVariableSchemaNameHelper } from '../../../../core/helper/get-variable-schema-name-helper'
+
+export function generateOneOfCode(schema: Schema, config: Config): string {
+  if (!schema.oneOf || schema.oneOf.length === 0) {
+    console.warn('not exists oneOf')
+    return 'z.any()'
+    }
+  
+    const zodSchemas = schema.oneOf.map((subSchema) => {
+      if (subSchema.$ref) {
+        const refParts = subSchema.$ref.split('/')
+        const refName = refParts[refParts.length - 1]
+        const schemaName = getVariableSchemaNameHelper(refName, config)
+        return schemaName
+      } else {
+        return generateZodSchema(config, subSchema)
+      }
+    })
+  
+    return `z.union([${zodSchemas.join(', ')}])`
+  }
