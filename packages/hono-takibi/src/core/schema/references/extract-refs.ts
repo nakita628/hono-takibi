f34bfb4586c82a3481b0bfd5ce65b2aa7a7cc9d6
nakita628 <nakita@ypm.llc>
2025-01-14 11:59:04 +0000

    fix

--- packages/hono-takibi/src/core/schema/references/extract-refs.ts
+++ packages/hono-takibi/src/core/schema/references/extract-refs.ts
@@ -1,27 +1,28 @@
 import type { Schema } from '../../../types'
-
-type TraversableObject = {
-  [key: string]: TraversableObject | string | number | boolean | null | undefined
-  $ref?: string
-}
+import { isRefObject } from '../../validator/is-ref-object'
 
 export function extractRefs(schema: Schema): string[] {
   const refs: string[] = []
 
-  const traverse = (obj: TraversableObject): void => {
-    if (typeof obj !== 'object' || obj === null) return
+  const traverse = (obj: unknown): void => {
+    if (!isRefObject(obj)) return
 
     for (const [key, value] of Object.entries(obj)) {
-      if (key === '$ref' && typeof value === 'string') {
-        const refParts = value.split('/')
-        const refName = refParts[refParts.length - 1]
-        refs.push(refName)
-      } else if (typeof value === 'object' && value !== null) {
-        traverse(value as TraversableObject)
+      if (key !== '$ref') {
+        if (isRefObject(value)) {
+          traverse(value)
+        }
+        continue
       }
+
+      if (typeof value !== 'string') continue
+
+      const refParts = value.split('/')
+      const refName = refParts[refParts.length - 1]
+      refs.push(refName)
     }
   }
 
-  traverse(schema as TraversableObject)
+  traverse(schema)
   return refs
 }
