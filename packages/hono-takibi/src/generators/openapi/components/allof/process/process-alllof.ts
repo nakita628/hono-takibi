ab0c61eb1f278e14acab8a3b1ef40416c0386d34
nakita628 <nakita@ypm.llc>
2025-01-15 10:31:02 +0000

    add processAllOf test

---
+++ packages/hono-takibi/src/generators/openapi/components/allof/process/process-alllof.ts
@@ -0,0 +1,31 @@
+import { Config } from '../../../../../config'
+import { isNullableSchema } from '../../../../../core/validator/is-nullable-schema'
+import { Schema } from '../../../../../types'
+import { generateZodSchemaFromSubSchema } from '../../../../zod/sub/generate-zod-schema-from-sub-schema'
+
+type Accumulator = {
+  nullable: boolean
+  schemas: string[]
+}
+
+/**
+ * Processes the `allOf` array, separating the `nullable` flag and the array of schemas.
+ *
+ * @param allOf - The `allOf` array.
+ * @param config - The configuration object.
+ * @returns An object containing the `nullable` flag and the generated array of schemas.
+ */
+export function processAllOf(allOf: Schema[], config: Config): Accumulator {
+  return allOf.reduce<Accumulator>(
+    (acc, subSchema) => {
+      if (isNullableSchema(subSchema)) {
+        acc.nullable = true
+        return acc
+      }
+      const zodSchema = generateZodSchemaFromSubSchema(subSchema, config)
+      acc.schemas.push(zodSchema)
+      return acc
+    },
+    { nullable: false, schemas: [] },
+  )
+}
