cc68084277e5913535e56d500baac3cdb72e779f
nakita628 <nakita@ypm.llc>
2025-01-15 02:40:16 +0000

    change type

--- packages/hono-takibi/src/types/index.ts
+++ packages/hono-takibi/src/types/index.ts
@@ -137,7 +137,7 @@ type SecurityRequirementObject = {
  * Operation definition
  */
 export type Operation = {
-  tags: string[]
+  tags?: string[]
   summary?: string
   description?: string
   operationId?: string
@@ -151,8 +151,9 @@ export type Operation = {
  * Response definition with description and content
  */
 export type ResponseDefinition = {
-  description: string
+  description?: string
   content?: Content
+  $ref?: string
   headers?: {
     [key: string]: {
       description?: string
@@ -187,6 +188,8 @@ export type Schema = {
   required?: string[]
   items?: Schema
   enum?: string[]
+  object?: {}
+  nullable?: boolean
   additionalProperties?: {
     type: Type
     format: Format
@@ -197,6 +200,9 @@ export type Schema = {
     wrapped?: boolean
   }
   security?: SecurityRequirementObject[]
+  oneOf?: Schema[]
+  allOf?: Schema[]
+  anyOf?: Schema[]
 }
 
 /**
