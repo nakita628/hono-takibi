695f1c8f24ea0c410f1c446f730a8944c6c5a573
nakita628 <nakita@ypm.llc>
2025-01-15 08:56:42 +0000

    add is-nullable-schema

---
+++ packages/hono-takibi/src/core/validator/is-nullable-schema.ts
@@ -0,0 +1,11 @@
+import type { Schema } from '../../types'
+
+/**
+ * Determines whether a given sub-schema is `nullable`.
+ *
+ * @param subSchema - The sub-schema to evaluate
+ * @returns `true` if `nullable` is set to `true` and it is the only key in the schema, otherwise `false`
+ */
+export function isNullableSchema(schema: Schema): boolean {
+  return 'nullable' in schema && schema.nullable === true && Object.keys(schema).length === 1
+}
