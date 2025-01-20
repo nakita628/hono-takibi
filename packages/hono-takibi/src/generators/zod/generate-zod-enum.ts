05b5cbadcff3fd04b55a5ea645d8fabd93271eda
nakita628 <nakita@ypm.llc>
2025-01-20 10:17:54 +0000

    .openapi move

--- packages/hono-takibi/src/generators/zod/generate-zod-enum.ts
+++ packages/hono-takibi/src/generators/zod/generate-zod-enum.ts
@@ -1,8 +1,10 @@
 import type { Schema } from '../../types'
+import { generateZodToOpenAPI } from './openapi/generate-zod-to-openapi'
 
 export function generateZodEnum(schema: Schema) {
   if (schema.example) {
-    return `z.enum(${JSON.stringify(schema.enum)}).openapi({example:${JSON.stringify(schema.example)}})`
+    const openapi_example = generateZodToOpenAPI(schema.example)
+    return `z.enum(${JSON.stringify(schema.enum)})${openapi_example}`
   }
   return `z.enum(${JSON.stringify(schema.enum)})`
 }
--- packages/hono-takibi/src/generators/zod/generate-zod-record-schema.test.ts
+++ packages/hono-takibi/src/generators/zod/generate-zod-record-schema.test.ts
@@ -1,26 +1,31 @@
 import { describe, expect, it } from 'vitest'
 import { generateZodRecordSchema } from './generate-zod-record-schema'
 import type { Format, Type } from '../../types'
+import type { Config } from '../../config'
+import { DEFAULT_CONFIG } from '../../data/test-data'
 
 const generateZodRecordSchemaTestCases: {
   additionalProperties: { type: Type; format: Format }
+  config: Config
   expected: string
 }[] = [
   {
     additionalProperties: { type: 'integer', format: 'int32' },
+    config: DEFAULT_CONFIG,
     expected: 'z.record(z.string(),z.number().int())',
   },
   {
     additionalProperties: { type: 'string', format: 'email' },
+    config: DEFAULT_CONFIG,
     expected: 'z.record(z.string(),z.string().email())',
   },
 ]
 
 describe('generateZodRecordSchema', () => {
   it.concurrent.each(generateZodRecordSchemaTestCases)(
-    'generateZodRecordSchema($additionalProperties) -> $expected',
-    async ({ additionalProperties, expected }) => {
-      const result = generateZodRecordSchema(additionalProperties)
+    'generateZodRecordSchema($additionalProperties, $config) -> $expected',
+    async ({ additionalProperties, config, expected }) => {
+      const result = generateZodRecordSchema(additionalProperties, config)
       expect(result).toBe(expected)
     },
   )
