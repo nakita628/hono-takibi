0a916a9877a70c45ee35e042764e1be2dc0ece6c
nakita628 <nakita@ypm.llc>
2025-01-26 09:07:50 +0000

    add generateDocs

---
+++ packages/hono-takibi/src/generators/openapi/docs/generate-docs.ts
@@ -0,0 +1,17 @@
+import type { OpenAPISpec } from '../../../types'
+
+export function generateDocs(openAPISpec: OpenAPISpec): {
+  openapi: string
+  info: OpenAPISpec['info']
+  servers: OpenAPISpec['servers']
+  externalDocs: OpenAPISpec['externalDocs']
+  tags: OpenAPISpec['tags']
+} {
+  return {
+    openapi: openAPISpec.openapi,
+    info: openAPISpec.info,
+    servers: openAPISpec.servers,
+    externalDocs: openAPISpec.externalDocs,
+    tags: openAPISpec.tags,
+  }
+}
