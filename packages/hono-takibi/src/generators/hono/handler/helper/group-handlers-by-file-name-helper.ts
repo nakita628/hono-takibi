27f050edca5f4f275e0d680aa6db5a6a4dc1aa6d
nakita628 <nakita@ypm.llc>
2025-01-26 05:34:47 +0000

    lint fix

--- packages/hono-takibi/src/generators/hono/handler/helper/group-handlers-by-file-name-helper.ts
+++ packages/hono-takibi/src/generators/hono/handler/helper/group-handlers-by-file-name-helper.ts
@@ -1,13 +1,15 @@
-import { HandlerOutput } from '../generate-zod-openapi-hono-handler'
+import type { HandlerOutput } from '../generate-zod-openapi-hono-handler'
 
 export function groupHandlersByFileNameHelper(handlers: HandlerOutput[]): HandlerOutput[] {
   const mergedMap = new Map<string, HandlerOutput>()
 
   for (const handler of handlers) {
     if (mergedMap.has(handler.fileName)) {
-      const existing = mergedMap.get(handler.fileName)!
-      existing.routeHandlerContents.push(...handler.routeHandlerContents)
-      existing.routeNames.push(...handler.routeNames)
+      const existing = mergedMap.get(handler.fileName)
+      if (existing) {
+        existing.routeHandlerContents.push(...handler.routeHandlerContents)
+        existing.routeNames.push(...handler.routeNames)
+      }
     } else {
       mergedMap.set(handler.fileName, {
         fileName: handler.fileName,
--- packages/hono-takibi/vitest.config.ts
+++ packages/hono-takibi/vitest.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    // include: ['**/src/**/*.test.ts'],
-    include: ['**/src/generators/hono/handler/helper/**/*.test.ts'],
-    // include: ['**/src/generators/handler/**/*.test.ts'],
+    include: ['**/src/**/*.test.ts'],
     exclude: ['**/node_modules'],
     coverage: {
       include: ['**/src/**/*.test.ts'],
