84654bfced5ec9680fa9fc67cd4d19c8cbb6a596
nakita628 <nakita@ypm.llc>
2025-01-26 10:13:46 +0000

    add getHandlerImports

---
+++ packages/hono-takibi/src/generators/hono/app/import/get-handler-imports.ts
@@ -0,0 +1,23 @@
+export function getHandlerImports(
+  handlerMaps: {
+    routeName: string
+    handlerName: string
+    path: string
+  }[],
+) {
+  const getHandlerImports: { [fileName: string]: string[] } = {}
+  for (const { handlerName, path } of handlerMaps) {
+    const path_name = path
+      .replace(/[\/{}-]/g, ' ')
+      .trim()
+      .split(/\s+/)[0]
+
+    const fileName = path_name.length === 0 ? 'index_handler.ts' : `${path_name}_handler.ts`
+
+    if (!getHandlerImports[fileName]) {
+      getHandlerImports[fileName] = []
+    }
+    getHandlerImports[fileName].push(handlerName)
+  }
+  return getHandlerImports
+}
