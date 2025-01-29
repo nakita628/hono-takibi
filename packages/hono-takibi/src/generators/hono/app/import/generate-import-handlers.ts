476a5163b279f9c487396de8d623d3664bec5d23
nakita628 <nakita@ypm.llc>
2025-01-29 10:13:22 +0000

    fix

--- packages/hono-takibi/src/generators/hono/app/import/generate-import-handlers.ts
+++ packages/hono-takibi/src/generators/hono/app/import/generate-import-handlers.ts
@@ -7,9 +7,13 @@ export function generateImportHandlers(
   const importHandlers: string[] = []
   for (const [fileName, handlers] of Object.entries(handlerImportsMap)) {
     const uniqueHandlers = Array.from(new Set(handlers))
-    importHandlers.push(
-      `import { ${uniqueHandlers.join(',')} } from './${config?.handler?.output}/${fileName}';`,
-    )
+    if (config?.handler?.output === true) {
+      const dirPath = config?.output?.replace(/\/[^/]+\.ts$/, '')
+      const handlerPath = dirPath === 'index.ts' ? 'handler' : `${dirPath}/handler`
+      importHandlers.push(
+        `import { ${uniqueHandlers.join(',')} } from './${handlerPath}/${fileName}';`,
+      )
+    }
   }
   return importHandlers
 }
