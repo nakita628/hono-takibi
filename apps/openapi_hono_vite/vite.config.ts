65f0b470da6764645fca7fe21ab47e82ba289ca1
nakita628 <nakita@ypm.llc>
2025-02-06 09:47:34 +0000

    add

--- apps/openapi_hono_vite/vite.config.ts
+++ apps/openapi_hono_vite/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import devServer from '@hono/vite-dev-server'
+import honoTakibiPlugin from 'hono-takibi/vite-plugin'
 
 export default defineConfig({
   plugins: [
     devServer({
       entry: 'src/index.ts',
+    }),
+    honoTakibiPlugin({
+      input: 'src/openapi.yaml',
+      output: 'src/openapi/index.ts',
+      packageManager: 'pnpm',
     }),
   ],
 })
