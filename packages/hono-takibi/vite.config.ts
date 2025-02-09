e0e43338c907fb153d3e6ad627e0e6115f5a687e
nakita628 <nakita@ypm.llc>
2025-02-09 09:59:30 +0000

    plugin fix

--- packages/hono-takibi/vite.config.ts
+++ packages/hono-takibi/vite.config.ts
@@ -2,11 +2,5 @@ import { defineConfig } from 'vite'
 import honoTakibiPlugin from './src/vite-plugin'
 
 export default defineConfig({
-  plugins: [
-    honoTakibiPlugin({
-      input: 'example/hono-rest-example.yaml',
-      output: 'routes/hono-rest-example.ts',
-      packageManager: 'pnpm',
-    }),
-  ],
+  plugins: [honoTakibiPlugin()],
 })
