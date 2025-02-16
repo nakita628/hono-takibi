39bca7cc1cb34174d916995717c86fff92542f93
nakita628 <nakita@ypm.llc>
2025-02-16 01:52:13 +0000

    add generateReferenceSchema

---
+++ packages/hono-takibi/src/generators/zod/reference/generate-reference-schema.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, it } from 'vitest'
+import { generateReferenceSchema } from './generate-reference-schema'
+import type { Schema } from '../../../types'
+import { DEFAULT_CONFIG, type Config } from '../../../config'
+
+const generateReferenceSchemaTestCases: {
+  schema: Schema
+  config: Config
+  expected: string
+}[] = [
+    {
+        schema: {
+            $ref: '#/components/schemas/User'
+        },
+        config: DEFAULT_CONFIG,
+        expected: 'UserSchema'
+    },
+    {
+        schema: { '$ref': '#/components/schemas/Error' },
+        config: DEFAULT_CONFIG,
+        expected: 'ErrorSchema'
+    },
+    {
+        schema: { '$ref': '#/components/schemas/Pet' },
+        config: DEFAULT_CONFIG,
+        expected: 'PetSchema'
+    },
+    {
+        schema: { '$ref': '#/components/schemas/ApiResponse' },
+        config: DEFAULT_CONFIG,
+        expected: 'ApiResponseSchema'
+    },
+    {
+        schema: { '$ref': '#/components/schemas/Order' },
+        config: DEFAULT_CONFIG,
+        expected: 'OrderSchema'
+    },  
+]
+
+describe('generateReferenceSchema', () => {
+    it.concurrent.each(generateReferenceSchemaTestCases)(
+        'generateReferenceSchema(%s, %s) -> %s',
+        ({ schema, config, expected }) => {
+            expect(generateReferenceSchema(schema, config)).toBe(expected)
+        }
+    )
+})
