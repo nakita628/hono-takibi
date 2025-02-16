e55b57d88c42a6c998b094ab0ee93ed1c717156b
nakita628 <nakita@ypm.llc>
2025-02-16 00:27:08 +0000

    change hono → zod-openapi-hono

--- packages/hono-takibi/src/generators/handler/generate-handler-name.ts
+++ packages/hono-takibi/src/generators/handler/generate-handler-name.ts
@@ -1,4 +1,4 @@
-import { generateRouteName } from '../hono/openapi/route/generate-route-name'
+import { generateRouteName } from '../zod-openapi-hono/openapi/route/generate-route-name'
 
 export function generateHandlerName(method: string, path: string) {
   return `${generateRouteName(method, path)}Handler`
--- packages/hono-takibi/src/generators/zod/generate-zod-object.ts
+++ packages/hono-takibi/src/generators/zod/generate-zod-object.ts
@@ -1,9 +1,9 @@
 import type { Schema } from '../../types'
 import type { Config } from '../../config'
 import { generateZodRecordSchema } from './generate-zod-record-schema'
-import { generateAllOfCode } from '../hono/openapi/component/allof/generate-allof-code'
-import { generateOneOfCode } from '../hono/openapi/component/oneof/generate-oneof-code'
-import { generateAnyOfCode } from '../hono/openapi/component/anyof/generate-anyof-code'
+import { generateAllOfCode } from '../zod-openapi-hono/openapi/component/allof/generate-allof-code'
+import { generateOneOfCode } from '../zod-openapi-hono/openapi/component/oneof/generate-oneof-code'
+import { generateAnyOfCode } from '../zod-openapi-hono/openapi/component/anyof/generate-anyof-code'
 import { generateZodPropertiesSchema } from './generate-zod-properties-schema'
 
 export function generateZodObject(schema: Schema, config: Config) {
