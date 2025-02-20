67805de6897a55e58f0f1968148925b95438f594
nakita628 <nakita@ypm.llc>
2025-02-20 09:02:20 +0000

    change generateZodNumberSchema

--- packages/hono-takibi/src/generators/zod/generate-zod-number-schema.ts
+++ packages/hono-takibi/src/generators/zod/generate-zod-number-schema.ts
@@ -11,6 +11,8 @@ type GenerateZodNumberSchemaParams = {
   maxLength?: number
   minimum?: number
   maximum?: number
+  exclusiveMinimum?: boolean
+  exclusiveMaximum?: boolean
   default?: DefaultValue
   example?: ExampleValue
   paramName?: string
@@ -32,15 +34,44 @@ export function generateZodNumberSchema(args: GenerateZodNumberSchemaParams): st
   if (args.minLength) validations.push(generateZodMin(args.minLength))
   // maxLength
   if (args.maxLength) validations.push(generateZodMax(args.maxLength))
-  // 0 falsy value
   // minimum
-  if (typeof args.minimum === 'number') validations.push(generateZodMin(args.minimum))
+  if (args.minimum) validations.push(generateZodMin(args.minimum))
   // maximum
-  if (typeof args.maximum === 'number') validations.push(generateZodMax(args.maximum))
+  if (args.maximum) validations.push(generateZodMax(args.maximum))
   // default
   if (args.default) validations.push(generateZodDefault(args.default))
   // example
   if (args.example)
     validations.push(generateZodToOpenAPI(args.example, args.paramName, args.isPath))
+
+  // 0 falsy value
+  // minimum === 0
+  // positive
+  if (args.minimum === 0 && args.exclusiveMinimum) {
+    validations.push('.positive()')
+  }
+  // nonpositive
+  if (args.minimum === 0 && !args.exclusiveMinimum) {
+    validations.push('.nonpositive()')
+  }
+  // negative
+  if (args.maximum === 0 && args.exclusiveMaximum) {
+    validations.push('.negative()')
+  }
+  // gt
+  if (args.minimum) {
+    if (args.minimum > 0 && args.exclusiveMinimum) {
+      const res = `.gt(${args.minimum})`
+      validations.push(res)
+    }
+  }
+
+  // lt
+  if (args.maximum) {
+    if (args.maximum > 0 && args.exclusiveMaximum) {
+      const res = `.lt(${args.maximum})`
+      validations.push(res)
+    }
+  }
   return validations.join('')
 }
