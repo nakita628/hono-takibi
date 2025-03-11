f1371ae1ecb463d1d0f37dc5459e369b7b0dd4a0
nakita628 <nakita@ypm.llc>
2025-03-11 09:23:58 +0000

    test fix

--- packages/hono-takibi/src/core/zod/get-zod-string-format.test.ts
+++ packages/hono-takibi/src/core/zod/get-zod-string-format.test.ts
@@ -1,36 +1,36 @@
+import type { FormatString } from '../../type'
 import { describe, expect, it } from 'vitest'
 import { getZodFormatString } from './get-zod-string-format'
-import type { FormatString } from '../../type'
 
-const getZodFormatStringTestCases: [FormatString, string][] = [
-  // ['max', '.max()'],
-  // ['min', '.min()'],
-  // ['length', '.length()'],
-  ['email', '.email()'],
-  ['uri', '.url()'],
-  ['emoji', '.emoji()'],
-  ['uuid', '.uuid()'],
-  ['nanoid', '.nanoid()'],
-  ['cuid', '.cuid()'],
-  ['cuid2', '.cuid2()'],
-  ['ulid', '.ulid()'],
-  ['ip', '.ip()'],
-  ['date-time', '.datetime()'],
-  ['cidr', '.cidr()'],
-  ['trim', '.trim()'],
-  ['toLowerCase', '.toLowerCase()'],
-  ['toUpperCase', '.toUpperCase()'],
-  ['date', '.date()'],
-  ['time', '.time()'],
-  ['duration', '.duration()'],
-  ['base64', '.base64()'],
-  ['binary', '.instanceof(Uint8Array)'],
+const getZodFormatStringTestCases: {
+  format: FormatString
+  expected: string
+}[] = [
+  { format: 'email', expected: '.email()' },
+  { format: 'uri', expected: '.url()' },
+  { format: 'emoji', expected: '.emoji()' },
+  { format: 'uuid', expected: '.uuid()' },
+  { format: 'nanoid', expected: '.nanoid()' },
+  { format: 'cuid', expected: '.cuid()' },
+  { format: 'cuid2', expected: '.cuid2()' },
+  { format: 'ulid', expected: '.ulid()' },
+  { format: 'ip', expected: '.ip()' },
+  { format: 'date-time', expected: '.datetime()' },
+  { format: 'cidr', expected: '.cidr()' },
+  { format: 'trim', expected: '.trim()' },
+  { format: 'toLowerCase', expected: '.toLowerCase()' },
+  { format: 'toUpperCase', expected: '.toUpperCase()' },
+  { format: 'date', expected: '.date()' },
+  { format: 'time', expected: '.time()' },
+  { format: 'duration', expected: '.duration()' },
+  { format: 'base64', expected: '.base64()' },
+  { format: 'binary', expected: '.instanceof(Uint8Array)' },
 ]
 
 describe('getZodStringFormatData', () => {
   it.concurrent.each(getZodFormatStringTestCases)(
     `getZodStringFormatData('%s') -> '%s'`,
-    (format, expected) => {
+    ({ format, expected }) => {
       const result = getZodFormatString(format)
       expect(result).toBe(expected)
     },
