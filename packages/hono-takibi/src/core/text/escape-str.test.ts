bd742da672b1db4b74051ec619c7c9ae8b32d666
nakita628 <nakita@ypm.llc>
2025-04-20 05:13:12 +0000

    test fix

--- packages/hono-takibi/src/core/text/escape-str.test.ts
+++ packages/hono-takibi/src/core/text/escape-str.test.ts
@@ -1,52 +1,46 @@
-import { describe, expect, it } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { escapeStr } from './escape-str'
 
-const escapeTestCases = [
-  {
-    str: "It's a test",
-    expected: "It\\'s a test",
-  },
-  {
-    str: "It's a test's test",
-    expected: "It\\'s a test\\'s test",
-  },
-  {
-    str: "'test string",
-    expected: "\\'test string",
-  },
-  {
-    str: "test string'",
-    expected: "test string\\'",
-  },
-  {
-    str: 'test string',
-    expected: 'test string',
-  },
-  {
-    str: "'",
-    expected: "\\'",
-  },
-  {
-    str: '',
-    expected: '',
-  },
-  {
-    str: 'test "string" with quotes',
-    expected: 'test "string" with quotes',
-  },
-  {
-    str: "Retrieve Santa's wishlist for Christmas.",
-    expected: "Retrieve Santa\\'s wishlist for Christmas.",
-  },
-  {
-    str: "Santa's wishlist.",
-    expected: "Santa\\'s wishlist.",
-  },
-]
+// Test run
+// pnpm vitest run ./src/core/text/escape-str.test.ts
 
-describe('escape', () => {
-  it.concurrent.each(escapeTestCases)('escapeStr($str) -> $expected', ({ str, expected }) => {
-    const result = escapeStr(str)
+describe('escape Test', () => {
+  test.concurrent(`escapeStr('') -> ''`, () => {
+    const result = escapeStr('')
+    const expected = ''
+    expect(result).toBe(expected)
+  })
+  test.concurrent(`escapeStr("'") -> "\\'"`, () => {
+    const result = escapeStr("'")
+    const expected = "\\'"
+    expect(result).toBe(expected)
+  })
+  test.concurrent(`escapeStr("'test") -> "\\'test"`, () => {
+    const result = escapeStr("'test")
+    const expected = "\\'test"
+    expect(result).toBe(expected)
+  })
+  test.concurrent(`escapeStr("It's a test") -> "It\\'s a test"`, () => {
+    const result = escapeStr("It's a test")
+    const expected = "It\\'s a test"
+    expect(result).toBe(expected)
+  })
+  test.concurrent(`escapeStr('test "string" with quotes') -> 'test "string" with quotes'`, () => {
+    const result = escapeStr('test "string" with quotes')
+    const expected = 'test "string" with quotes'
+    expect(result).toBe(expected)
+  })
+  test.concurrent(
+    `escapeStr("Retrieve Santa's wishlist for Christmas.") -> "Retrieve Santa\\'s wishlist for Christmas."`,
+    () => {
+      const result = escapeStr("Retrieve Santa's wishlist for Christmas.")
+      const expected = "Retrieve Santa\\'s wishlist for Christmas."
+      expect(result).toBe(expected)
+    },
+  )
+  test.concurrent(`escapeStr("Santa's wishlist.") -> "Santa\\'s wishlist."`, () => {
+    const result = escapeStr("Santa's wishlist.")
+    const expected = "Santa\\'s wishlist."
     expect(result).toBe(expected)
   })
 })
--- packages/hono-takibi/src/core/text/regex-pattern.test.ts
+++ packages/hono-takibi/src/core/text/regex-pattern.test.ts
@@ -1,39 +1,44 @@
-import { describe, expect, it } from 'vitest'
+import { describe, test, expect, it } from 'vitest'
 import { regexPattern } from './regex-pattern'
 
-const regexPatternTestCases = [
-  {
-    pattern: '^[a-z]+$',
-    expected: '/^[a-z]+$/',
-  },
-  {
-    pattern: '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$',
-    expected: '/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$/',
-  },
-  {
-    pattern: '^#[0-9a-fA-F]{6}$',
-    expected: '/^#[0-9a-fA-F]{6}$/',
-  },
-  {
-    pattern: '^(https?:\\/\\/)?[\\w.-]+\\.[a-zA-Z]{2,}([\\/\\w .-]*)*\\/?$',
-    expected: '/^(https?:\\/\\/)?[\\w.-]+\\.[a-zA-Z]{2,}([\\/\\w .-]*)*\\/?$/',
-  },
-  {
-    pattern: '^\\d{4}-\\d{2}-\\d{2}$',
-    expected: '/^\\d{4}-\\d{2}-\\d{2}$/',
-  },
-  {
-    pattern: '^\\d{2}/\\d{2}$',
-    expected: '/^\\d{2}\\/\\d{2}$/',
-  },
-]
+// Test run
+// pnpm vitest run ./src/core/text/regex-pattern.test.ts
 
-describe('regexPattern', () => {
-  it.concurrent.each(regexPatternTestCases)(
-    'regexPattern($pattern) -> $expected',
-    ({ pattern, expected }) => {
-      const result = regexPattern(pattern)
+describe('regexPattern Test', () => {
+  test.concurrent(`regexPattern('^[a-z]+$') -> '/^[a-z]+$/'`, () => {
+    const result = regexPattern('^[a-z]+$')
+    const expected = '/^[a-z]+$/'
+    expect(result).toBe(expected)
+  })
+  test.concurrent(
+    `regexPattern('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$') -> '/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$/'`,
+    () => {
+      const result = regexPattern('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$')
+      const expected = '/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z|a-z]{2,}$/'
       expect(result).toBe(expected)
     },
   )
+  test.concurrent(`regexPattern('^#[0-9a-fA-F]{6}$') -> '/^#[0-9a-fA-F]{6}$/'`, () => {
+    const result = regexPattern('^#[0-9a-fA-F]{6}$')
+    const expected = '/^#[0-9a-fA-F]{6}$/'
+    expect(result).toBe(expected)
+  })
+  test.concurrent(
+    `regexPattern('^(https?:\\/\\/)?[\\w.-]+\\.[a-zA-Z]{2,}([\\/\\w .-]*)*\\/?$') -> '/^(https?:\\/\\/)?[\\w.-]+\\.[a-zA-Z]{2,}([\\/\\w .-]*)*\\/?$/'`,
+    () => {
+      const result = regexPattern('^(https?:\\/\\/)?[\\w.-]+\\.[a-zA-Z]{2,}([\\/\\w .-]*)*\\/?$')
+      const expected = '/^(https?:\\/\\/)?[\\w.-]+\\.[a-zA-Z]{2,}([\\/\\w .-]*)*\\/?$/'
+      expect(result).toBe(expected)
+    },
+  )
+  test.concurrent(`regexPattern('^\\d{4}-\\d{2}-\\d{2}$') -> '/^\\d{4}-\\d{2}-\\d{2}$/'`, () => {
+    const result = regexPattern('^\\d{4}-\\d{2}-\\d{2}$')
+    const expected = '/^\\d{4}-\\d{2}-\\d{2}$/'
+    expect(result).toBe(expected)
+  })
+  test.concurrent(`regexPattern('^\\d{2}/\\d{2}$') -> '/^\\d{2}\\/\\d{2}$/'`, () => {
+    const result = regexPattern('^\\d{2}/\\d{2}$')
+    const expected = '/^\\d{2}\\/\\d{2}$/'
+    expect(result).toBe(expected)
+  })
 })
