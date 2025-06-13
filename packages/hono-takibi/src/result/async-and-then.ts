b5007e4ff4fdf857f7e600db742b430030f23d6b
nakita628 <nakita@ypm.llc>
2025-06-13 10:03:30 +0000

    async-and-then

---
+++ packages/hono-takibi/src/result/async-and-then.ts
@@ -0,0 +1,12 @@
+import type { Result } from './types.js'
+
+export async function asyncAndThen<A, E, B>(
+  res: Result<A, E>,
+  f: (a: A) => Promise<Result<B, E>>,
+): Promise<Result<B, E>> {
+  if (res.ok) {
+    return await f(res.value)
+  } else {
+    return res
+  }
+}
