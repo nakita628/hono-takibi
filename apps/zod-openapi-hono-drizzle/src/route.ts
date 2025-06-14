497651164537b35c53954eae8a7c8672ccf50b84
nakita628 <nakita@ypm.llc>
2025-06-14 07:32:56 +0000

    fix openapi

--- apps/zod-openapi-hono-drizzle/src/route.ts
+++ apps/zod-openapi-hono-drizzle/src/route.ts
@@ -56,7 +56,13 @@ export const postPostsRoute = createRoute({
   responses: {
     201: {
       description: 'Post successfully created.',
-      content: { 'application/json': { schema: ErrorSchema } },
+      content: {
+        'application/json': {
+          schema: z.object({
+            message: z.string().openapi({ example: 'Post created successfully.' }),
+          }),
+        },
+      },
     },
     400: {
       description: 'Invalid request due to bad input.',
@@ -82,26 +88,13 @@ export const getPostsRoute = createRoute({
         z.coerce
           .number()
           .int()
-          .min(0)
-          .default(1)
-          .openapi({
-            param: { in: 'query', name: 'page', required: false },
-            example: 1,
-            description: 'The page number to retrieve. Must be a positive integer. Defaults to 1.',
-          }),
+          .openapi({ param: { in: 'query', name: 'page', required: false } }),
       ),
       rows: z.string().pipe(
         z.coerce
           .number()
           .int()
-          .min(0)
-          .default(10)
-          .openapi({
-            param: { in: 'query', name: 'rows', required: false },
-            example: 10,
-            description:
-              'The number of posts per page. Must be a positive integer. Defaults to 10.',
-          }),
+          .openapi({ param: { in: 'query', name: 'rows', required: false } }),
       ),
     }),
   },
@@ -146,7 +139,10 @@ export const putPostsIdRoute = createRoute({
       id: z
         .string()
         .uuid()
-        .openapi({ param: { in: 'path', name: 'id', required: true } }),
+        .openapi({
+          param: { in: 'path', name: 'id', required: true },
+          description: 'Unique identifier of the post.',
+        }),
     }),
   },
   responses: {
