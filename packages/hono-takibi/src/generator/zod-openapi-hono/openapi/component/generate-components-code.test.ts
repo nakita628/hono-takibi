8bff9027e07e92f5ccf9f1492d530e1796af4cc2
nakita628 <nakita@ypm.llc>
2025-06-20 09:22:11 +0000

    fix test

--- packages/hono-takibi/src/generator/zod-openapi-hono/openapi/component/generate-components-code.test.ts
+++ packages/hono-takibi/src/generator/zod-openapi-hono/openapi/component/generate-components-code.test.ts
@@ -5,238 +5,51 @@ import type { Components } from '../../../../openapi/index.js'
 // Test run
 // pnpm vitest run ./src/generator/zod-openapi-hono/openapi/component/generate-components-code.test.ts
 
-// const testComponents: Components = {
-//   schemas: {
-//     Test: {
-//       type: 'object',
-//       required: ['test'],
-//       properties: {
-//         test: {
-//           type: 'string',
-//         },
-//       },
-//     },
-//   },
-// }
+const testComponents: Components = {
+  schemas: {
+    Test: {
+      type: 'object',
+      required: ['test'],
+      properties: {
+        test: {
+          type: 'string',
+        },
+      },
+    },
+  },
+}
 
-// describe('generateComponentsCode Test', () => {
-//   // 1
-//   // {
-//   //   schema: { name: 'PascalCase', export: true },
-//   //   type: { name: 'PascalCase', export: true }
-//   // },
-//   it.concurrent('schema name PascalCase export true type PascalCase export true', () => {
-//     const result = generateComponentsCode(testComponents, true, true)
-//     const expected = `export const TestSchema = z.object({test:z.string()}).openapi('Test')
+describe('generateComponentsCode Test', () => {
+  // #1: exportSchema=true, exportType=true
+  it.concurrent('exportSchema=true, exportType=true', () => {
+    const result = generateComponentsCode(testComponents, true, true)
+    const expected = `export const TestSchema = z.object({test:z.string()}).openapi('Test')
 
-// export type Test = z.infer<typeof TestSchema>`
-//     expect(result).toBe(expected)
-//   })
-//   // 2
-//   // {
-//   //   schema: { name: 'PascalCase', export: true },
-//   //   type: { name: 'PascalCase', export: false }
-//   // },
-//   it.concurrent('schema name PascalCase export true type not output export false', () => {
-//     const result = generateComponentsCode(testComponents, true, false)
-//     const expected = `export const TestSchema = z.object({test:z.string()}).openapi('Test')
+export type Test = z.infer<typeof TestSchema>`
+    expect(result).toBe(expected)
+  })
+  // #2: exportSchema=true, exportType=false
+  it.concurrent('exportSchema=true, exportType=false', () => {
+    const result = generateComponentsCode(testComponents, true, false)
+    const expected = `export const TestSchema = z.object({test:z.string()}).openapi('Test')
 
-// `
+`
+    expect(result).toBe(expected)
+  })
+  // #3: exportSchema=false, exportType=true
+  it.concurrent('exportSchema=false, exportType=true', () => {
+    const result = generateComponentsCode(testComponents, false, true)
+    const expected = `const TestSchema = z.object({test:z.string()}).openapi('Test')
 
-//     expect(result).toBe(expected)
-//   })
-//   // 3
-//   // {
-//   //   schema: { name: 'PascalCase', export: true },
-//   //   type: { name: 'camelCase', export: true }
-//   // },
-//   it.concurrent('schema name PascalCase export true type camelCase export true', () => {
-//     const result = generateComponentsCode(testComponents, true, true)
-//     const expected = `export const TestSchema = z.object({test:z.string()}).openapi('Test')
+export type Test = z.infer<typeof TestSchema>`
+    expect(result).toBe(expected)
+  })
+  // #4: exportSchema=false, exportType=false
+  it.concurrent('exportSchema=false, exportType=false', () => {
+    const result = generateComponentsCode(testComponents, false, false)
+    const expected = `const TestSchema = z.object({test:z.string()}).openapi('Test')
 
-// export type test = z.infer<typeof TestSchema>`
-
-//     expect(result).toBe(expected)
-//   })
-//   // 4
-//   // {
-//   //   schema: { name: 'PascalCase', export: true },
-//   //   type: { name: 'camelCase', export: false }
-//   // },
-//   it.concurrent('schema name PascalCase export true type not output export false', () => {
-//     const result = generateComponentsCode(testComponents, true, false)
-//     const expected = `export const TestSchema = z.object({test:z.string()}).openapi('Test')
-
-// `
-
-//     expect(result).toBe(expected)
-//   })
-//   // 5
-//   // {
-//   //   schema: { name: 'PascalCase', export: false },
-//   //   type: { name: 'PascalCase', export: true }
-//   // },
-//   it.concurrent('schema name PascalCase export false type PascalCase export true', () => {
-//     const result = generateComponentsCode(testComponents, false, true)
-//     const expected = `const TestSchema = z.object({test:z.string()}).openapi('Test')
-
-// export type Test = z.infer<typeof TestSchema>`
-
-//     expect(result).toBe(expected)
-//   })
-//   // 6
-//   // {
-//   //   schema: { name: 'PascalCase', export: false },
-//   //   type: { name: 'PascalCase', export: false }
-//   // },
-//   it.concurrent('schema name PascalCase export false type not output export false', () => {
-//     const result = generateComponentsCode(testComponents, false, false)
-//     const expected = `const TestSchema = z.object({test:z.string()}).openapi('Test')
-
-// `
-
-//     expect(result).toBe(expected)
-//   })
-//   // 7
-//   // {
-//   //   schema: { name: 'PascalCase', export: false },
-//   //   type: { name: 'camelCase', export: true }
-//   // },
-//   it.concurrent('schema name PascalCase export false type camelCase export true', () => {
-//     const result = generateComponentsCode(testComponents, false, true)
-//     const expected = `const TestSchema = z.object({test:z.string()}).openapi('Test')
-
-// export type test = z.infer<typeof TestSchema>`
-
-//     expect(result).toBe(expected)
-//   })
-//   // 8
-//   // {
-//   //   schema: { name: 'PascalCase', export: false },
-//   //   type: { name: 'camelCase', export: false }
-//   // },
-//   it.concurrent('schema name PascalCase export false type not output export false', () => {
-//     const result = generateComponentsCode(testComponents, false, false)
-//     const expected = `const TestSchema = z.object({test:z.string()}).openapi('Test')
-
-// `
-
-//     expect(result).toBe(expected)
-//   })
-//   // 9
-//   // {
-//   //   schema: { name: 'camelCase', export: true },
-//   //   type: { name: 'PascalCase', export: true }
-//   // },
-//   it.concurrent('schema name camelCase export true type PascalCase export true', () => {
-//     const result = generateComponentsCode(testComponents, true, true)
-//     const expected = `export const testSchema = z.object({test:z.string()}).openapi('Test')
-
-// export type Test = z.infer<typeof testSchema>`
-
-//     expect(result).toBe(expected)
-//   })
-//   // 10
-//   // {
-//   //   schema: { name: 'camelCase', export: true },
-//   //   type: { name: 'PascalCase', export: false }
-//   // },
-//   it.concurrent('schema name camelCase export true type not output export false', () => {
-//     const result = generateComponentsCode(testComponents, true, false)
-//     const expected = `export const testSchema = z.object({test:z.string()}).openapi('Test')
-
-// `
-
-//     expect(result).toBe(expected)
-//   })
-//   // 11
-//   // {
-//   //   schema: { name: 'camelCase', export: true },
-//   //   type: { name: 'camelCase', export: true }
-//   // },
-//   it.concurrent('schema name camelCase export true type camelCase export true', () => {
-//     const result = generateComponentsCode(testComponents, true, true)
-//     const expected = `export const testSchema = z.object({test:z.string()}).openapi('Test')
-
-// export type test = z.infer<typeof testSchema>`
-
-//     expect(result).toBe(expected)
-//   })
-//   // 12
-//   // {
-//   //   schema: { name: 'camelCase', export: true },
-//   //   type: { name: 'camelCase', export: false }
-//   // },
-//   it.concurrent('schema name camelCase export true type not output export false', () => {
-//     const result = generateComponentsCode(testComponents, true, false)
-//     const expected = `export const testSchema = z.object({test:z.string()}).openapi('Test')
-
-// `
-
-//     expect(result).toBe(expected)
-//   })
-
-//   // 13
-//   // {
-//   //   schema: { name: 'camelCase', export: false },
-//   //   type: { name: 'PascalCase', export: true }
-//   // },
-//   it.concurrent('schema name camelCase export false type PascalCase export true', () => {
-//     const result = generateComponentsCode(testComponents, false, true)
-//     const expected = `const testSchema = z.object({test:z.string()}).openapi('Test')
-
-// export type Test = z.infer<typeof testSchema>`
-
-//     expect(result).toBe(expected)
-//   })
-
-//   // 14
-//   // {
-//   //   schema: { name: 'camelCase', export: false },
-//   //   type: { name: 'PascalCase', export: false }
-//   // },
-//   it.concurrent('schema name camelCase export false type not output export false', () => {
-//     const result = generateComponentsCode(testComponents, false, false)
-//     const expected = `const testSchema = z.object({test:z.string()}).openapi('Test')
-
-// `
-
-//     expect(result).toBe(expected)
-//   })
-
-//   // 15
-//   // {
-//   //   schema: { name: 'camelCase', export: false },
-//   //   type: { name: 'camelCase', export: true }
-//   // },
-//   it.concurrent('schema name camelCase export false type camelCase export true', () => {
-//     const result = generateComponentsCode(testComponents, false, true)
-//     const expected = `const testSchema = z.object({test:z.string()}).openapi('Test')
-
-// export type test = z.infer<typeof testSchema>`
-
-//     expect(result).toBe(expected)
-//   })
-
-//   // 16
-//   // {
-//   //   schema: { name: 'camelCase', export: false },
-//   //   type: { name: 'camelCase', export: false }
-//   // }
-//   it.concurrent('schema name camelCase export false type not output export false', () => {
-//     const result = generateComponentsCode(testComponents, false, false)
-//     const expected = `const testSchema = z.object({test:z.string()}).openapi('Test')
-
-// `
-
-//     expect(result).toBe(expected)
-//   })
-//   // 17
-//   // schema empty
-//   it.concurrent(`schema empty -> ''`, () => {
-//     const result = generateComponentsCode({}, false, false)
-//     const expected = ''
-
-//     expect(result).toBe(expected)
-//   })
-// })
+`
+    expect(result).toBe(expected)
+  })
+})
