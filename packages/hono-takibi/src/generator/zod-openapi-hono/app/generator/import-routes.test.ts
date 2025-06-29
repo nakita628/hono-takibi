2e305f343b4f1bed906c7a857f481e65800f3533
nakita628 <nakita@ypm.llc>
2025-06-29 06:24:22 +0000

    add

--- packages/hono-takibi/src/generator/zod-openapi-hono/app/generator/import-routes.test.ts
+++ packages/hono-takibi/src/generator/zod-openapi-hono/app/generator/import-routes.test.ts
@@ -4,43 +4,14 @@ import { importRoutes } from '.'
 // Test run
 // pnpm vitest run ./src/generator/zod-openapi-hono/app/generator/import-routes.test.ts
 
-const importRoutesTestCases = [
-  {
-    importsMap: {
-      'routes/pet-store.ts': [
-        'putPetRoute',
-        'postPetRoute',
-        'getPetFindByStatusRoute',
-        'getPetFindByTagsRoute',
-        'getPetPetIdRoute',
-        'postPetPetIdRoute',
-        'deletePetPetIdRoute',
-        'postPetPetIdUploadImageRoute',
-        'getStoreInventoryRoute',
-        'postStoreOrderRoute',
-        'getStoreOrderOrderIdRoute',
-        'deleteStoreOrderOrderIdRoute',
-        'postUserRoute',
-        'postUserCreateWithListRoute',
-        'getUserLoginRoute',
-        'getUserLogoutRoute',
-        'getUserUsernameRoute',
-        'putUserUsernameRoute',
-        'deleteUserUsernameRoute',
-      ],
-    },
-    expected: [
-      "import { putPetRoute,postPetRoute,getPetFindByStatusRoute,getPetFindByTagsRoute,getPetPetIdRoute,postPetPetIdRoute,deletePetPetIdRoute,postPetPetIdUploadImageRoute,getStoreInventoryRoute,postStoreOrderRoute,getStoreOrderOrderIdRoute,deleteStoreOrderOrderIdRoute,postUserRoute,postUserCreateWithListRoute,getUserLoginRoute,getUserLogoutRoute,getUserUsernameRoute,putUserUsernameRoute,deleteUserUsernameRoute } from './routes/pet-store.ts';",
-    ],
-  },
-]
-
 describe('generateImportRoutes', () => {
-  it.concurrent.each(importRoutesTestCases)(
-    'generateImportRoutes($importsMap) -> $expected',
-    ({ importsMap, expected }) => {
-      const result = importRoutes(importsMap)
-      expect(result).toEqual(expected)
-    },
-  )
+  it.concurrent('importRoutes Test', () => {
+    const result = importRoutes({
+      'routes.ts': ['getHonoRoute', 'getHonoXRoute', 'getZodOpenapiHonoRoute'],
+    })
+    const expected = [
+      "import { getHonoRoute,getHonoXRoute,getZodOpenapiHonoRoute } from './routes.ts';",
+    ]
+    expect(result).toStrictEqual(expected)
+  })
 })
--- packages/hono-takibi/vitest.config.ts
+++ packages/hono-takibi/vitest.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     includeSource: ['src/**/*.ts'],
-    include: ['src/**/*.ts'],
+    include: ['src/**/*.test.ts'],
     coverage: {
       include: ['src/**/*.ts'],
       all: true,
-    }
+    },
   },
 })
