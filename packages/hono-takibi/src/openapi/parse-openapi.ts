5c4efefdf4045027583cef0bb5b23c0ed29cd8a3
nakita628 <nakita@ypm.llc>
2025-06-30 11:33:08 +0000

    typespec add

--- packages/hono-takibi/src/openapi/parse-openapi.ts
+++ packages/hono-takibi/src/openapi/parse-openapi.ts
@@ -2,6 +2,7 @@ import SwaggerParser from '@apidevtools/swagger-parser'
 import type { OpenAPI } from './index.js'
 import type { Result } from '../result/index.js'
 import { ok, err } from '../result/index.js'
+import { typeSpecToOpenAPI } from '../typespec/index.js'
 
 /**
  * Parses an OpenAPI specification from a string input.
@@ -10,6 +11,11 @@ import { ok, err } from '../result/index.js'
  */
 export async function parseOpenAPI(input: string): Promise<Result<OpenAPI, string>> {
   try {
+    if (input.endsWith('.tsp')) {
+      const tsp = await typeSpecToOpenAPI(input)
+      const spec = (await SwaggerParser.parse(tsp as unknown as string)) as OpenAPI
+      return ok(spec)
+    }
     const spec = (await SwaggerParser.parse(input)) as OpenAPI
     return ok(spec)
   } catch (e) {
