659512957c971f37be551c0f4a5a7198b870b125
nakita628 <nakita@ypm.llc>
2025-07-06 10:23:05 +0000

    delete comment

--- apps/zod-openapi-hono-drizzle/drizzle.config.ts
+++ apps/zod-openapi-hono-drizzle/drizzle.config.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
-// 環境変数に基づいてデータベースファイルを選択
 const dbFileName =
   process.env.NODE_ENV === 'test' ? 'file:test.db' : process.env.DB_FILE_NAME || 'file:local.db'
 
