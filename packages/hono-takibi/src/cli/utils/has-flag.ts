156ce7590d4029cac44fa63dbcfc09618146a5bd
nakita628 <nakita@ypm.llc>
2025-07-13 03:43:22 +0000

    utils

--- packages/hono-takibi/src/cli/utils/has-flag.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @param args - The array of strings to check for the flag
- * @param f
- */
-
-export function hasFlag(args: readonly string[], f: string) {
-  return args.includes(f)
-}
--- packages/hono-takibi/src/cli/utils/slice-argv.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @param argv - The command line arguments array
- */
-export function sliceArgv(argv: readonly string[]): string[] {
-  return argv.slice(2)
-}
