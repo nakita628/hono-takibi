3caf8e841bc0b82ffc9a95f7f2c53d8824a5f1a2
nakita628 <nakita@ypm.llc>
2025-07-24 04:34:28 +0000

    fix

--- packages/hono-takibi/src/generator/zod-openapi-hono/openapi/components/components-code.ts
+++ packages/hono-takibi/src/generator/zod-openapi-hono/openapi/components/components-code.ts
@@ -43,7 +43,7 @@ export function componentsCode(
       // 4.3 generate zod schema definition
       return zodToOpenAPISchema(schemaName, zodSchema, exportSchema, exportType)
     })
-    .join('')
+    .join('\n\n')
   // 5. return code
   return schemaDefinitions
 }
