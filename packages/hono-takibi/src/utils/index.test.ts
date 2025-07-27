02f02271409125dc29e712d596e9c114b2d2d714
nakita628 <nakita@ypm.llc>
2025-07-27 06:22:18 +0000

    fix

--- packages/hono-takibi/src/utils/index.test.ts
+++ packages/hono-takibi/src/utils/index.test.ts
@@ -850,24 +850,24 @@ describe('utils', () => {
    *  Zod Chain Optimisation
    * ========================================================================== */
   // removeMaxIfLtExists
-  describe('stripMaxIfLtExist', () => {
-    it.concurrent(`stripMaxIfLtExist('z.number().max(1).lt(1)', 1) -> 'z.number().lt(1)'`, () => {
+  describe('removeMaxIfLtExists', () => {
+    it.concurrent(`removeMaxIfLtExists('z.number().max(1).lt(1)', 1) -> 'z.number().lt(1)'`, () => {
       const result = removeMaxIfLtExists('z.number().max(1).lt(1)', 1)
       const expected = 'z.number().lt(1)'
       expect(result).toBe(expected)
     })
   })
   // removeMinIfGtExists
-  describe('stripMinIfgtExist', () => {
-    it.concurrent(`stripMinIfgtExist('z.number().min(1).gt(1)', 1) -> 'z.number().gt(1)'`, () => {
+  describe('removeMinIfGtExists', () => {
+    it.concurrent(`removeMinIfGtExists('z.number().min(1).gt(1)', 1) -> 'z.number().gt(1)'`, () => {
       const result = removeMinIfGtExists('z.number().min(1).gt(1)', 1)
       const expected = 'z.number().gt(1)'
       expect(result).toBe(expected)
     })
   })
   // removeMinMaxIfEqual
-  describe('stripMinMaxExist', () => {
-    it.concurrent(`stripMinMaxExist('z.string().min(1).max(1)', 1, 1) -> 'z.string()'`, () => {
+  describe('removeMinMaxIfEqual', () => {
+    it.concurrent(`removeMinMaxIfEqual('z.string().min(1).max(1)', 1, 1) -> 'z.string()'`, () => {
       const result = removeMinMaxIfEqual('z.string().min(1).max(1)', 1, 1)
       const expected = 'z.string()'
       expect(result).toBe(expected)
