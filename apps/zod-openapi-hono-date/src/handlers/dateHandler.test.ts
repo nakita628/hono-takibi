3f752310cb31ec91bf0060cbbd4eafb4999d3ae4
nakita628 <nakita@ypm.llc>
2025-08-08 05:16:09 +0000

    fix examplecode

--- apps/zod-openapi-hono-date/src/handlers/dateHandler.test.ts
+++ apps/zod-openapi-hono-date/src/handlers/dateHandler.test.ts
@@ -2,70 +2,90 @@ import { testClient } from 'hono/testing'
 import { describe, expect, it } from 'vitest'
 import { api } from '..'
 
+// Test run
+// pnpm vitest run ./src/handlers/dateHandler.test.ts
+
 // test client
 const test = testClient(api)
 
-describe('Hono Zod OpenAPI Test', () => {
+describe('dateHandler', () => {
   describe('postDateHandler', () => {
-    it('should return 200 with valid date', async () => {
-      const res = await test.date.$post({
-        json: { date: new Date('2023-10-01') },
+    describe('POST /date', () => {
+      it.concurrent('200', async () => {
+        const res = await test.date.$post({
+          json: { date: new Date('2023-10-01') },
+        })
+        const input = await res.json()
+        expect(input).toStrictEqual({ date: '2023-10-01T00:00:00.000Z' })
+        expect(res.status).toBe(200)
       })
-      const input = await res.json()
-      expect(input).toStrictEqual({ date: '2023-10-01T00:00:00.000Z' })
-      expect(res.status).toBe(200)
-    })
-    it('should return 200 with valid date', async () => {
-      const res = await test.date.$post({
-        json: { date: '2022-01-12T06:15:00.000Z' },
+      it.concurrent('200', async () => {
+        const res = await test.date.$post({
+          json: { date: '2022-01-12T06:15:00.000Z' },
+        })
+        const input = await res.json()
+        expect(input).toStrictEqual({ date: '2022-01-12T06:15:00.000Z' })
+        expect(res.status).toBe(200)
       })
-      const input = await res.json()
-      expect(input).toStrictEqual({ date: '2022-01-12T06:15:00.000Z' })
-      expect(res.status).toBe(200)
+      it.concurrent('200', async () => {
+        const res = await test.date.$post({
+          json: { date: '2023-10-01' },
+        })
+        expect(res.status).toBe(200)
+      })
+      it.concurrent('400', async () => {
+        const res = await test.date.$post({
+          json: { date: 'invalid-date' },
+        })
+        expect(res.status).toBe(400)
+      })
     })
   })
 
   describe('postIsoDateHandler', () => {
-    it('should return 200 with valid ISO date', async () => {
-      const res = await test.date['iso-date'].$post({
-        json: { iso_date: '2023-10-01' },
+    describe('POST /date/iso-date', () => {
+      it('200', async () => {
+        const res = await test.date['iso-date'].$post({
+          json: { iso_date: '2023-10-01' },
+        })
+        const input = await res.json()
+        expect(input).toStrictEqual({ iso_date: '2023-10-01' })
+        expect(res.status).toBe(200)
       })
-      const input = await res.json()
-      expect(input).toStrictEqual({ iso_date: '2023-10-01' })
-      expect(res.status).toBe(200)
-    })
 
-    it('should return 200 with valid ISO date', async () => {
-      const res = await test.date['iso-date'].$post({
-        json: { iso_date: '2022-01-12T06:15:00.000Z' },
+      it('400', async () => {
+        const res = await test.date['iso-date'].$post({
+          json: { iso_date: '2022-01-12T06:15:00.000Z' },
+        })
+        expect(res.status).toBe(400)
       })
-      const input = await res.json()
-      expect(res.status).toBe(400)
-    })
 
-    it('should return 400 with invalid ISO date', async () => {
-      const res = await test.date['iso-date'].$post({
-        json: { iso_date: 'invalid-date' },
+      it('400', async () => {
+        const res = await test.date['iso-date'].$post({
+          json: { iso_date: 'invalid-date' },
+        })
+        expect(res.status).toBe(400)
       })
-      expect(res.status).toBe(400)
     })
   })
 
   describe('postIsoDatetimeHandler', () => {
-    it('should return 200 with valid ISO datetime', async () => {
-      const res = await test.date['iso-datetime'].$post({
-        json: { iso_datetime: '2023-10-01T12:00:00Z' },
+    describe('POST /date/iso-datetime', () => {
+      it('200', async () => {
+        const res = await test.date['iso-datetime'].$post({
+          json: { iso_datetime: '2023-10-01T12:00:00Z' },
+        })
+        const input = await res.json()
+        expect(input).toStrictEqual({ iso_datetime: '2023-10-01T12:00:00Z' })
+        expect(res.status).toBe(200)
       })
-      const input = await res.json()
-      expect(input).toStrictEqual({ iso_datetime: '2023-10-01T12:00:00Z' })
-      expect(res.status).toBe(200)
-    })
 
-    it('should return 400 with invalid ISO datetime', async () => {
-      const res = await test.date['iso-datetime'].$post({
-        json: { iso_datetime: '2023-10-01' },
+      it('400', async () => {
+        const res = await test.date['iso-datetime'].$post({
+          json: { iso_datetime: '2023-10-01' },
+        })
+        expect(res.status).toBe(400)
       })
-      expect(res.status).toBe(400)
     })
   })
 })
