a2a1b07fc407bbc8035f222fe921c1d5c8b0d342
nakita628 <nakita@ypm.llc>
2025-08-10 05:25:27 +0000

    docs

---
+++ docs/hono/src/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+
+const app = new Hono()
+
+app.get('/*', serveStatic({ root: './typedoc' }))
+
+const port = 3000
+console.log(`Server is running on http://localhost:${port}`)
+
+serve({
+    port: 3000,
+    fetch: app.fetch
+})
+
+export default app
