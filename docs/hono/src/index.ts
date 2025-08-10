7df0cabd9c7d2f5a0f75737e9db3e1fb82003d04
nakita628 <nakita@ypm.llc>
2025-08-10 07:00:10 +0000

    docs

--- docs/hono/src/index.ts
+++ docs/hono/src/index.ts
@@ -1,6 +1,6 @@
-import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
 import { serveStatic } from '@hono/node-server/serve-static'
+import { Hono } from 'hono'
 
 const app = new Hono()
 
@@ -10,8 +10,8 @@ const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
 
 serve({
-    port: 3000,
-    fetch: app.fetch
+  port: 3000,
+  fetch: app.fetch,
 })
 
 export default app
