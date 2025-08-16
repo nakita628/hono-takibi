f561415893327e5414b1cbebb04940115a7a0efa
nakita628 <nakita@ypm.llc>
2025-08-16 08:43:46 +0000

    add

--- packages/hono-takibi/src/helper/get-route-maps.ts
+++ packages/hono-takibi/src/helper/get-route-maps.ts
@@ -10,9 +10,18 @@ import { methodPath } from '../utils/index.js'
 export function getRouteMaps(
   openapi: OpenAPI,
 ): { routeName: string; handlerName: string; path: string }[] {
+  const isHttpMethod = (
+    v: string,
+  ): v is 'get' | 'put' | 'post' | 'delete' | 'patch' | 'options' | 'head' | 'trace' => {
+    for (const m of ['get', 'put', 'post', 'delete', 'patch', 'options', 'head', 'trace']) {
+      if (m === v) return true
+    }
+    return false
+  }
   const paths = openapi.paths
   const routeMappings = Object.entries(paths).flatMap(([path, pathItem]) => {
     return Object.entries(pathItem).flatMap(([method]) => {
+      if (!isHttpMethod(method)) return []
       return {
         routeName: `${methodPath(method, path)}Route`,
         handlerName: `${methodPath(method, path)}RouteHandler`,
