518ed606398ef018d1a06924f23cfb3315ad33ec
nakita628 <mini.bg.pro.takibi.n@gmail.com>
2025-12-30 06:45:28 +0000

    add

--- packages/hono-takibi/src/generator/zod-openapi-hono/openapi/components/headers.ts
+++ packages/hono-takibi/src/generator/zod-openapi-hono/openapi/components/headers.ts
@@ -1,38 +1,49 @@
 import { makeContent } from '../../../../helper/components.js'
 import type { Components, Header, Reference } from '../../../../openapi/index.js'
-import { ensureSuffix, toIdentifierPascalCase } from '../../../../utils/index.js'
+import {
+  ensureSuffix,
+  toIdentifierPascalCase,
+  zodToOpenAPISchema,
+} from '../../../../utils/index.js'
 import { zodToOpenAPI } from '../../../zod-to-openapi/index.js'
+
+const isReference = (v: unknown): v is Reference =>
+  typeof v === 'object' && v !== null && '$ref' in v
+
+const isHeader = (v: unknown): v is Header =>
+  typeof v === 'object' && v !== null && !('$ref' in v)
 
 export function headers(
   components: Components,
   exportHeaders: boolean,
   exportHeadersTypes: boolean,
 ) {
-  const isReference = (v: unknown): v is Reference =>
-    typeof v === 'object' && v !== null && '$ref' in v
-  const isHeader = (v: unknown): v is Header =>
-    typeof v === 'object' && v !== null && !('$ref' in v)
   const { headers } = components
   if (!headers) return ''
 
   return Object.entries(headers)
     .map(([k, header]) => {
-      const zInfer = exportHeadersTypes
-        ? `\n\nexport type ${toIdentifierPascalCase(ensureSuffix(k, 'Header'))} = z.infer<typeof ${toIdentifierPascalCase(ensureSuffix(k, 'Header'))}>`
-        : ''
+      const schemaName = toIdentifierPascalCase(ensureSuffix(k, 'Header'))
 
       if (isReference(header) && header.$ref) {
         const refName = header.$ref.split('/').pop() ?? k
-        return `${exportHeaders ? 'export const' : 'const'} ${toIdentifierPascalCase(ensureSuffix(k, 'Header'))}=${toIdentifierPascalCase(ensureSuffix(refName, 'Header'))}${zInfer}`
+        const refSchema = toIdentifierPascalCase(ensureSuffix(refName, 'Header'))
+        return zodToOpenAPISchema(schemaName, refSchema, exportHeaders, exportHeadersTypes, true)
       }
       if (isHeader(header)) {
         if (header.schema) {
           const schema = zodToOpenAPI(header.schema, { headers: header })
-          return `${exportHeaders ? 'export const' : 'const'} ${toIdentifierPascalCase(ensureSuffix(k, 'Header'))}=${schema}${zInfer}`
+          return zodToOpenAPISchema(schemaName, schema, exportHeaders, exportHeadersTypes, true)
         }
         if (header.content) {
           const content = makeContent(header.content)
-          return `${exportHeaders ? 'export const' : 'const'} ${toIdentifierPascalCase(ensureSuffix(k, 'Header'))}={${content}}${zInfer}`
+          return zodToOpenAPISchema(
+            schemaName,
+            `{${content}}`,
+            exportHeaders,
+            exportHeadersTypes,
+            true,
+          )
         }
       }
       return ''
