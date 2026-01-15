4a121a30d7d489fd85e475d5e0f09d594a207b46
nakita628 <mini.bg.pro.takibi.n@gmail.com>
2026-01-15 09:18:51 +0000

    add

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,21 +3,13 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    includeSource: ['packages/**/src/**/*.ts'],
+    includeSource: ['packages/**/src/**/*.ts', 'apps/**/src/**/*.ts'],
     alias: {
       '@/': `${path.resolve(__dirname, 'apps/nakita628-todo/src')}/`,
     },
     env: {
       DB_FILE_NAME: 'file:local.db',
       DATABASE_URL: 'file:./dev.db',
-    },
-  },
-  resolve: {
-    alias: {
-      '@hono/zod-openapi': path.resolve(
-        __dirname,
-        'packages/hono-takibi/node_modules/@hono/zod-openapi',
-      ),
     },
   },
 })
