95abfb1e19ab08985310c4230ad6356f558dcaf0
nakita628 <mini.bg.pro.takibi.n@gmail.com>
2026-01-29 04:53:22 +0000

    simple setting

--- apps/nakita628-tanstack-router-query-todo/vite.config.ts
+++ apps/nakita628-tanstack-router-query-todo/vite.config.ts
@@ -1,47 +1,19 @@
-import build from '@hono/vite-build/cloudflare-workers'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
-export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
-    return {
-      plugins: [
-        tailwindcss(),
-        tsconfigPaths(),
-      ],
-      build: {
-        rollupOptions: {
-          input: './src/main.tsx',
-          output: {
-            entryFileNames: 'static/main.js',
-            assetFileNames: 'static/main.css',
-          },
-        },
-      },
-    }
-  }
-  return {
-    ssr: {
-      external: ['react', 'react-dom'],
-    },
-    plugins: [
-      build({
-        outputDir: 'server-build',
-      }),
-      devServer({
-        adapter,
-        entry: 'src/index.tsx',
-      }),
-      tanstackRouter({
-        target: 'react',
-        autoCodeSplitting: true,
-      }),
-      tailwindcss(),
-      tsconfigPaths(),
-    ],
-  }
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    cloudflare(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+  ],
 })
