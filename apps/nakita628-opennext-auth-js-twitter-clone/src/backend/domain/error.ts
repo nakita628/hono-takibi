6bff121b1f7be50fd092c236b2ffafcd97cb5c68
nakita628 <mini.bg.pro.takibi.n@gmail.com>
2026-02-12 07:54:22 +0000

    add domain

---
+++ apps/nakita628-opennext-auth-js-twitter-clone/src/backend/domain/error.ts
@@ -0,0 +1,17 @@
+export class DatabaseError extends Error {
+    constructor(
+      message: string,
+      public readonly cause?: unknown,
+    ) {
+      super(message)
+      this.name = 'DatabaseError'
+    }
+  }
+  
+  export class DataNotFoundError extends Error {
+    constructor(message: string) {
+      super(message)
+      this.name = 'DataNotFoundError'
+    }
+  }
+  
