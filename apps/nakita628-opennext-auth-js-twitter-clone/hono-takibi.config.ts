a67bfde4ce5a0cfb1ba778dbacd7b7a34794760d
nakita628 <mini.bg.pro.takibi.n@gmail.com>
2026-02-12 06:10:46 +0000

    add

---
+++ apps/nakita628-opennext-auth-js-twitter-clone/hono-takibi.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'hono-takibi/config'
+
+export default defineConfig({
+  input: 'main.tsp',
+  'zod-openapi': {
+    output: 'src/backend/routes/index.ts',
+    template: true,
+    test: true,
+    pathAlias: '@/backend',
+  },
+})
