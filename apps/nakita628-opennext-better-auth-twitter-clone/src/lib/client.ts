9b116dd183e18e383d2fb5977ed959cfdea57509
nakita628 <takibilabdev@gmail.com>
2026-06-13 03:11:44 +0000

    delete

--- apps/nakita628-opennext-better-auth-twitter-clone/src/lib/client.ts
+++
@@ -1,9 +0,0 @@
-import { hc } from 'hono/client'
-
-import type { api } from '@/server'
-
-type Client = ReturnType<typeof hc<typeof api>>
-
-const hcWithType = (...args: Parameters<typeof hc>): Client => hc<typeof api>(...args)
-
-export const client = hcWithType('/').api
--- apps/nakita628-react-router-swr-todo/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths(), cloudflare()],
-})
