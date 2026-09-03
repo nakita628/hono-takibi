68a6e2c1b8a2f85f0daca5fc66c7977977347e07
nakita628 <takibilabdev@gmail.com>
2026-09-03 11:20:20 +0000

    delete

--- packages/hono-takibi/tmp-config-test/hono-takibi.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  input: 'openapi.json',
-  routes: { output: 'src/routes', split: true },
-  components: {
-    examples: { output: 'src/examples', split: true, import: '@/examples' },
-  },
-}
